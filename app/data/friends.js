f4d0eaf2b3f2df1bf624f1d7648449c0aaa3be50
Ashley Louitan <jataveius@aol.com>
2018-01-31 23:11:36 -0500

    add coding

---
+++ app/data/friends.js
@@ -0,0 +1,75 @@
+// ===============================================================================
+// DATA
+// ===============================================================================
+var friends = [
+{
+    "name":"Kevin Hart",
+    "photo":"https://upload.wikimedia.org/wikipedia/commons/b/ba/Kevin_Hart_2014.jpg",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  },
+
+  {
+    "name":"Dwayne Johnson",
+    "photo":"https://media1.popsugar-assets.com/files/thumbor/F0rHR3gbCtCQMI_itbRXIYQU7Uc/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2016/11/19/947/n/1922398/ad4ab23c5830c79bac6634.46030135_edit_img_image_42203217_1473123600/i/Dwayne-Johnson-Fast-8-Feud-Details.jpg",
+    "scores":[
+        5,
+        4,
+        4,
+        4,
+        5,
+        3,
+        2,
+        5,
+        4,
+        5
+      ]
+  }, 
+
+  {
+    "name":"Tiffany Haddish",
+    "photo":"https://peopledotcom.files.wordpress.com/2017/07/tiffany-haddish.jpg",
+    "scores":[
+        4,
+        5,
+        2,
+        4,
+        5,
+        4,
+        3,
+        5,
+        4,
+        2
+      ]
+  }, 
+
+  {
+    "name":"Denzel Wishington",
+    "photo":"https://pixel.nymag.com/imgs/daily/vulture/2016/09/22/22-denzel-washington.w710.h473.2x.jpg",
+    "scores":[
+        2,
+        4,
+        4,
+        4,
+        3,
+        3,
+        4,
+        5,
+        2,
+        3
+      ]
+  }, 
+]
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friends;
